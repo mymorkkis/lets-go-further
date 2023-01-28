1d776b4435857ce7568c64ed7c1fc32ca532a580
mymorkkis <mvsy@protonmail.com>
2023-01-28 15:22:33 +0000

    Chapter 15.3: sending activation tokens

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"net/http"
+	"time"
 
 	"github.com/mymorkkis/lets-go-further-json-api/internal/data"
 	"github.com/mymorkkis/lets-go-further-json-api/internal/validator"
@@ -52,14 +53,24 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 		return
 	}
 
+	// TODO Reduce this time to 1 hour and update user_welcome template
+	token, err := app.models.Tokens.New(user.ID, 3*24*time.Hour, data.ScopeActivation)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
 	app.background(func() {
-		err = app.mailer.Send(user.Email, "user_welcome.html", user)
-		if err != nil {
-			app.logger.PrintError(err, nil)
+		data := map[string]any{
+			"activationToken": token.Plaintext,
+			"userID":          user.ID,
+		}
+
+		if app.mailer.Send(user.Email, "user_welcome.html", data); err == nil {
+			properties := map[string]string{"email": user.Email}
+			app.logger.PrintInfo("sent welcome email to user", properties)
 		} else {
-			app.logger.PrintInfo("sent welcome email to user", map[string]string{
-				"email": user.Email,
-			})
+			app.logger.PrintError(err, nil)
 		}
 	})
 
